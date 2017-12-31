3943a85f1cd49cd6fd2ac20603ae0851850f08e0
Felix Roske <f.roske@roskenet.de>
2017-12-31 10:55:48 +0100

    Initial commit

---
+++ src/main/java/de/roskenet/epub/FxEbupBuilderApplication.java
@@ -0,0 +1,12 @@
+package de.roskenet.epub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxEbupBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxEbupBuilderApplication.class, args);
+	}
+}
---
+++ src/test/java/de/roskenet/epub/FxEbupBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package de.roskenet.epub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FxEbupBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
